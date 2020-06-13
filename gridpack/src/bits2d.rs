0efdc747ad3cc130c501329625749fa28d7da186
Jonas Jensen <jbj@knef.dk>
2020-06-13 14:09:21 +0200

    Autoformat

--- gridpack/src/bits2d.rs
+++ gridpack/src/bits2d.rs
@@ -13,21 +13,26 @@ pub struct Bits2D {
 impl Bits2D {
     pub fn new(length1: u32, length2: u32) -> Self {
         Bits2D {
-            elem: FixedBitSet::with_capacity(
-                      (length1 as usize) * (length2 as usize)),
+            elem: FixedBitSet::with_capacity((length1 as usize) * (length2 as usize)),
             length1: length1,
             length2: length2,
         }
     }
 
-    pub fn length1(&self) -> u32 { self.length1 }
-    pub fn length2(&self) -> u32 { self.length2 }
+    pub fn length1(&self) -> u32 {
+        self.length1
+    }
+    pub fn length2(&self) -> u32 {
+        self.length2
+    }
 
     // this function can't be replaced with IndexMut because that doesn't allow
     // a "virtual Bool"
     pub fn assign(&mut self, x1: u32, x2: u32, value: bool) {
-        self.elem.set((x1 as usize) + (x2 as usize)*(self.length1 as usize),
-                      value);
+        self.elem.set(
+            (x1 as usize) + (x2 as usize) * (self.length1 as usize),
+            value,
+        );
     }
 
     pub fn set(&mut self, x1: u32, x2: u32) {
@@ -38,7 +43,6 @@ impl Bits2D {
 impl Index<(u32, u32)> for Bits2D {
     type Output = bool;
     fn index(&self, (x1, x2): (u32, u32)) -> &bool {
-        &self.elem[(x1 as usize) + (x2 as usize)*(self.length1 as usize)]
+        &self.elem[(x1 as usize) + (x2 as usize) * (self.length1 as usize)]
     }
 }
-
