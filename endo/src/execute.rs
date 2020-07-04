5d0c73bac334f659496d4d6052892a807528581b
Jonas Jensen <jbj@knef.dk>
2020-07-04 16:15:45 +0200

    stub execution

---
+++ endo/src/execute.rs
@@ -0,0 +1,24 @@
+use crate::dna::DNA;
+
+pub fn execute(dna: &mut DNA, mut rna_sink: impl FnMut(DNA)) {
+    while step(dna, &mut rna_sink).is_ok() {
+        // do nothing
+    }
+}
+
+fn step(dna: &mut DNA, mut rna_sink: &mut impl FnMut(DNA)) -> Result<(), ()> {
+    let p = pattern(dna, rna_sink)?;
+    let t = template(dna, rna_sink)?;
+    // TODO: matchreplace
+    Ok(())
+}
+
+fn pattern(dna: &mut DNA, mut rna_sink: &mut impl FnMut(DNA)) -> Result<DNA, ()> {
+    rna_sink("ICFPICFPI".into());
+    Ok(DNA::default())
+}
+
+fn template(dna: &mut DNA, rna_sink: &mut impl FnMut(DNA)) -> Result<DNA, ()> {
+    rna_sink("CFPICFPIC".into());
+    Err(())
+}
--- endo/src/main.rs
+++ endo/src/main.rs
@@ -1,7 +1,12 @@
 #![allow(unused)]
 
 mod dna;
+mod execute;
+
+use dna::DNA;
 
 fn main() {
     println!("Hello, world!");
+    let mut dna: DNA = "ICFP".into();
+    execute::execute(&mut dna, |dna| println!("Got some RNA: {:?}", dna))
 }
