1637713cc6bcf6b9f4d7dfc57cd142f62b8da74a
Johan Rosenkilde <jsrn@jsrn.dk>
2020-07-04 16:35:34 +0200

    Threaded rna consumption

--- endo/src/main.rs
+++ endo/src/main.rs
@@ -5,8 +5,26 @@ mod execute;
 
 use dna::DNA;
 
+use crossbeam_channel::unbounded;
+
 fn main() {
     println!("Hello, world!");
     let mut dna: DNA = "ICFP".into();
-    execute::execute(&mut dna, |dna| println!("Got some RNA: {:?}", dna))
+
+    // Create a channel of unbounded capacity.
+    let (s, r) = unbounded();
+
+    let thr = std::thread::spawn(move || {
+        loop {
+            let orna = r.recv().unwrap();
+            match orna {
+                None => return,
+                Some(dna) => println!("Got some RNA: {:?}", dna),
+            }
+        }
+    });
+
+    execute::execute(&mut dna, |dna| s.send(Some(dna)).unwrap());
+    s.send(None).unwrap();
+    thr.join();
 }
