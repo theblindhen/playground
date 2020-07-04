a8ef1138aab11b435f101a41ecac039d2169566f
Jonas Jensen <jbj@knef.dk>
2020-07-04 11:03:19 +0200

    notes

--- gridpack/src/main.rs
+++ gridpack/src/main.rs
@@ -2,6 +2,19 @@
 mod bits2d;
 mod model;
 
+// What should be the rules for the game?
+// - Fill as many squares as possible with full lookahead?
+//   - That requires a SAT solver or metaheuristic.
+// - Lookahead 1
+//   - Machine learning?
+//   - What space are we then searching? Not A*.
+// - Require placing adjacent to existing fill?
+//   - That's more like a game, but it's a strange rule.
+//   - That surface can grow large.
+// - Have a concept of a player position and moves?
+//   - Even more like a game.
+//   - Can accomodate some machine learning?
+
 fn main() {
     let stair_step = model::shapes::stair_step();
     let ell = model::shapes::ell();
